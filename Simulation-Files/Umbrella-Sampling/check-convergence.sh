85f1986a84c37bc409d1038434ac067f847253d5
ANUPAMA SHARMA <anusharma.jan1995@gmail.com>
2025-09-08 09:55:34 +0530

    Update check-convergence.sh

--- Simulation-Files/Umbrella-Sampling/check-convergence.sh
+++ Simulation-Files/Umbrella-Sampling/check-convergence.sh
@@ -7,3 +7,12 @@ mv tpr-files.dat-sorted tpr-files.dat
 cp tpr-files.dat pullf-files.dat
 sed 's/\.tpr$/_pullf.xvg/' tpr-files.dat > pullf-files.dat
 rm tpr-files-clean.dat
+
+#====================================================================================#
+# Run Command 
+#--> bash check-convergence.sh
+#--> gmx wham -it tpr-files.dat -if pullf-files.dat -o -hist -unit kCal
+#--> ls -lstr
+#------------------------------------------------------------------------------------#
+# You'll generate two files namely "histo.xvg" and "profile.xvg"
+#====================================================================================#
